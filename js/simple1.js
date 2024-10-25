0e95e90cf579b905419d3b406661a653ddabac75
arrabotuharinathreddy <arrabotuharinathreddy@gmail.com>
2024-10-25 11:33:01 +0530

    html

---
+++ js/simple1.js
@@ -0,0 +1,7 @@
+let collegeName="Scient Engg College";
+console.log(collegeName.startsWith("Scient"))
+console.log(collegeName.startsWith("College"))
+
+console.log(collegeName.endsWith("College"))
+console.log(collegeName.endsWith("scient"))
+
